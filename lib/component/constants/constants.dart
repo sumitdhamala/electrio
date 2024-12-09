f1507e285d5c383e1f70ddace1b355ec012f8f95
Arachnoid <140705176+sumitdhamala@users.noreply.github.com>
2024-12-09 20:18:01 +0545

    Update constants.dart

--- lib/component/constants/constants.dart
+++ lib/component/constants/constants.dart
@@ -6,6 +6,6 @@ const url = "http://localhost:8000";
 const mblurl = "http://192.168.100.8:8000";
 
 class ApiKeys {
-  static const String khaltiPublicKey = "ce6cd7269ab14d5ca2e9ec9a1ccd29a6";
+  static const String khaltiPublicKey = "9f74423ecb2f4131aef333d24d65a04e";
   static const String khaltiSecretKey = "46e2baab49e847fcbbc7f98b8aa0224b";
 }
